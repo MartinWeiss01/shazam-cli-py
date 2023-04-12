365efefd880c073a8df05d3121b68a369700261c
xkellner <xkellner@mendelu.cz>
2023-04-13 00:00:17 +0200

    discogs testing

--- test.py
+++ test.py
@@ -1,25 +1,41 @@
 import discogs_client
 
+#esults = d.search('Stockholm By Night', type='release')
+# vyhledání shody podle názvů umělce, skladby a alba
 
 d = discogs_client.Client('MyApp', user_token='ZxOYlSZlyJrjGopmaEoHzbwbdayDeqgcnLZaKbkY')
 
+def print_song_info(song):
+    results = d.search(song, type='release')
+    release = results[0]
+    print(release.data.keys())
 
-results = d.search('Duality', artist='Slipknot')
-# vyhledání shody podle názvů umělce, skladby a alba
+
+    track = None
+
+    for t in release.tracklist:
+        if t.title == song:
+            track = t
+            break
 
 
-release = results[0]
-#print(release.data.keys())
-#print(release)
 
-t = release.title
-f = str(t)
-f = f.split("-",1)
-print(f[0])
 
-print(release.title)
-print("".join(release.genres))
-print("Rok vydání: " + str(release.year))
+    print("Artist: " + release.artists[0].name)
+    print("Title(Album): " + release.title)
+    print("Label: " + release.labels[0].name)
+    print("Release year: " + str(release.year))
+    print("Genre: " + str(release.genres))
+    print(f"Length: {track.duration}")
+    print("Apple Music ID:" + str(release.id))
+
+
+
+print_song_info('Zick Zack')
+
+
+
+
 
 
 
