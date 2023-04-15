cbd9dfa36fb62582787a2d088e9b3071aed8bfa3
xkellner <xkellner@mendelu.cz>
2023-04-15 13:13:34 +0200

    Adding is_strict & is_preview parametres

--- identification.py
+++ identification.py
@@ -7,19 +7,19 @@ ALLOWED_FILE_TYPES = ['.aac', '.aiff', '.dsf', '.flac', '.m4a', '.mp3', '.ogg', 
 def is_file_type_allowed(file_extension):
   return file_extension in ALLOWED_FILE_TYPES
 
-def identify_file(file_path, is_rename, is_preview):
+def identify_file(file_path, is_rename, is_preview, is_strict, discogs_api):
   file_extension = os.path.splitext(file_path)[1]
   if is_file_type_allowed(file_extension):
-    shazamapp = ShazamAppTrack(file_path, is_rename, is_preview)
+    shazamapp = ShazamAppTrack(file_path, is_rename, is_preview, is_strict, discogs_api)
     shazamapp.identify_track()
   else:
     print(f"{FormattedString().WARNING}[ShazamApp] Skipping {file_extension} file: {file_path}{FormattedString().END}")
 
 # Browse directory_path and identify all found files (recursively if specified)
-def identify_folder_files(directory_path, is_recursive, is_rename, is_preview):
+def identify_folder_files(directory_path, is_recursive, is_rename, is_preview,is_strict,discogs_api):
     for root, dirs, files in os.walk(directory_path):
         for filename in files:
             file_path = os.path.join(root, filename)
-            identify_file(file_path, is_rename, is_preview)
+            identify_file(file_path, is_rename, is_preview, is_strict, discogs_api)
         if not is_recursive:
             break
